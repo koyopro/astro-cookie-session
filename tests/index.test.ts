2c1021a28f03c60ba26d208a7c8bc652240f8352
koyopro <koyopro2.0@gmail.com>
2024-09-28 02:35:45 +0900

    refactor test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,47 +1,39 @@
 import { Session } from "../src";
 
-test("Session", () => {
-  const data: Record<string, any> = {};
-  const expectChangeSession = (fn: () => void) => {
-    const before = data["___session"];
-    fn();
-    expect(data["___session"]).not.toBe(before);
-  };
-  const mockAstroCookies = {
-    get: (key: string) => data[key],
-    set: (key: string, value: string | Record<string, any>) => {
-      data[key] = value;
-    },
-  } as any;
-  const session = Session.from({ cookies: mockAstroCookies });
+let data: Record<string, any> = {};
+const mockAstroCookies = {
+  get: (key: string) => ({ value: data[key] }),
+  set: (key: string, value: string | Record<string, any>) => {
+    data[key] = value;
+  },
+} as any;
 
+beforeEach(() => (data = {}));
+
+test("Session", () => {
   expect(data["___session"]).toBeUndefined();
 
-  expectChangeSession(() => {
+  const session = Session.from({ cookies: mockAstroCookies });
+
+  expectSessionChange(() => {
     session.set("keyForString", "myValue");
     session["keyForNumber"] = 33;
   });
   expect(session.get("keyForString")).toBe("myValue");
   expect(session["keyForNumber"]).toBe(33);
 
-  expectChangeSession(() => session.delete("keyForString"));
+  expectSessionChange(() => session.delete("keyForString"));
   expect(session["keyForString"]).toBeUndefined();
   expect(session["keyForNumber"]).toBe(33);
 
-  expectChangeSession(() => session.clear());
+  expectSessionChange(() => session.clear());
   expect(session["keyForNumber"]).toBeUndefined();
 });
 
-test("restore", () => {
-  const data: Record<string, any> = {};
-  const mockAstroCookies = {
-    get: (key: string) => ({ value: data[key] }),
-    set: (key: string, value: string | Record<string, any>) => {
-      data[key] = value;
-    },
-  } as any;
-  const session1 = Session.from({ cookies: mockAstroCookies });
+test("restore Session", () => {
+  expect(data["___session"]).toBeUndefined();
 
+  const session1 = Session.from({ cookies: mockAstroCookies });
   session1.set("keyForString", "myValue");
   session1["keyForNumber"] = 33;
 
@@ -49,3 +41,9 @@ test("restore", () => {
   expect(session2.get("keyForString")).toBe("myValue");
   expect(session2["keyForNumber"]).toBe(33);
 });
+
+const expectSessionChange = (fn: () => void) => {
+  const before = data["___session"];
+  fn();
+  expect(data["___session"]).not.toBe(before);
+};
