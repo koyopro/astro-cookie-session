1cfb1402a692fa90f963e007e696fa00e7df6846
koyopro <koyopro2.0@gmail.com>
2024-09-28 02:07:13 +0900

    implement Session

---
+++ src/index.ts
@@ -0,0 +1,90 @@
+import type { AstroCookies } from "astro";
+// @ts-ignore
+import pkg from "jsonwebtoken";
+const { sign, verify, JsonWebTokenError } = pkg;
+
+type Context = {
+  cookies: AstroCookies;
+};
+
+export class Session {
+  key = "___session";
+  data: Record<string, any> = {};
+  [key: string]: any;
+
+  constructor(private context: Context) {
+    this.secret = this.getSecret();
+    const jwt = this.context.cookies.get(this.key)?.value;
+    this.restore(jwt);
+  }
+
+  static from(context: Context) {
+    return new Proxy(new Session(context), {
+      get(target, key, receiver) {
+        if (["get", "set", "delete", "clear"].includes(key as string)) {
+          return Reflect.get(target, key, receiver).bind(target);
+        }
+        return target.get(key as string);
+      },
+      set(target, key, value) {
+        target.set(key as string, value);
+        return true;
+      },
+    });
+  }
+
+  protected getSecret() {
+    const secretKeyBase = process.env.SECRET_KEY_BASE;
+    if (secretKeyBase) {
+      return secretKeyBase;
+    }
+    switch (process.env.NODE_ENV || "") {
+      case "test":
+        return "test-secret-key-base";
+      case "development":
+        console.warn("Warning: process.env.SECRET_KEY_BASE is not set");
+        return "development-secret-key-base";
+      default:
+        throw new Error("SECRET_KEY_BASE is not set");
+    }
+  }
+
+  set(key: string, value: any) {
+    this.data[key] = value;
+    this.save();
+  }
+
+  get(key: string) {
+    return this.data[key];
+  }
+
+  delete(key: string) {
+    delete this.data[key];
+    this.save();
+  }
+
+  clear() {
+    this.data = {};
+    this.save();
+  }
+
+  protected restore(jwt: string | undefined) {
+    if (!jwt) return;
+    try {
+      const v = verify(jwt, this.secret, { algorithms: ["HS256"] });
+      if (!v || typeof v !== "object") return;
+      this.data = v;
+    } catch (e: unknown) {
+      if (e instanceof JsonWebTokenError) {
+        // ignore
+      } else {
+        throw e;
+      }
+    }
+  }
+
+  protected save() {
+    const jwt = sign(this.data, this.secret, { algorithm: "HS256" });
+    this.context.cookies.set(this.key, jwt);
+  }
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,33 @@
+import { Session } from "../src";
 
-test('', () => {
+test("Session", () => {
+  const data: Record<string, any> = {};
+  const expectChangeSession = (fn: () => void) => {
+    const before = data["___session"];
+    fn();
+    expect(data["___session"]).not.toBe(before);
+  };
+  const mockAstroCookies = {
+    get: (key: string) => data[key],
+    set: (key: string, value: string | Record<string, any>) => {
+      data[key] = value;
+    },
+  } as any;
+  const session = Session.from({ cookies: mockAstroCookies });
 
-})
+  expect(data["___session"]).toBeUndefined();
+
+  expectChangeSession(() => {
+    session.set("keyForString", "myValue");
+    session["keyForNumber"] = 33;
+  });
+  expect(session.get("keyForString")).toBe("myValue");
+  expect(session["keyForNumber"]).toBe(33);
+
+  expectChangeSession(() => session.delete("keyForString"));
+  expect(session["keyForString"]).toBeUndefined();
+  expect(session["keyForNumber"]).toBe(33);
+
+  expectChangeSession(() => session.clear());
+  expect(session["keyForNumber"]).toBeUndefined();
+});
