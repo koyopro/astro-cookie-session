970255c98cf85a74fd4bff41562644821c8b3e1b
koyopro <koyopro2.0@gmail.com>
2024-10-09 02:21:52 +0900

    use flash in example

--- example/src/pages/signout.ts
+++ example/src/pages/signout.ts
@@ -5,7 +5,10 @@ import { getSession } from "../sessions";
 export const POST: APIRoute = async ({ cookies, redirect }) => {
   const session = getSession(cookies); // Prepare the session
 
-  session.delete('userId'); // Remove the user ID from the session
+  session.delete("userId"); // Remove the user ID from the session
 
-  return redirect('/');;
-}
+  session.flash.set("success", "You have successfully logged out."); // Set the flash message
+  // session.flash["success"] = "You have successfully logged out."; // You can also use this code
+
+  return redirect("/");
+};
